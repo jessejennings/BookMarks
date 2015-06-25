0ee41bc6b99c01cd72b5a39b17f3c2b2da3b3426
jessejennings <jessejenn@gmail.com>
2015-06-25 10:29:28 -0700

    Create a new incoming controller with mailgun

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,8 +3,15 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
+    app = App.find_by(url: request.env['HTTP_ORIGIN'])
+    @event = app.events.build(event_params)
 
-
-
+    if app.nil?
+      render json: "Invalid Application", status: :unprocessable_entity
+    elsif @event.save
+      render json: @event, status: :created
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
   end
 end
